75c9157e9e3603877f2bb7754db9f685ea18fe04
shounak873 <shounakaero@gmail.com>
2022-02-17 17:05:36 -0500

    delete existing solution files

--- looprms.py
+++ looprms.py
@@ -1,17 +1,27 @@
 
-import os
 import subprocess
+import os
+import os.path
+import glob
+import shutil
 
 
-# pathpkg = os.getcwd()
-# dir1 = "build/FastGlobalRegistration"
-# dir2 = "dataset"
-#
-# pathexec = os.path.join(pathpkg, dir1)
-# pathdata = os.path.join(pathpkg, dir2)
+def remove_thing(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        os.remove(path)
+
+
+def empty_directory(path):
+    for i in glob.glob(os.path.join(path, '*')):
+        remove_thing(i)
 
 # delete existing files from that folder since we are appending
-# proc0 = subprocess.run(["rm", "results/Srko/*"])
+
+
+empty_directory('results/Srko')
+
 
 for i in range(25):
     for j in range(10):
@@ -32,5 +42,5 @@ for i in range(25):
                                 "dataset/pairwise_no_noise_" +
                                 str(i+1) + "_rot_05/output_" + str(j+1) + ".txt",
                                 "results/Srko/no_noise_eval_collection_" + str(i+1) + ".txt"])
-        # proc.wait()
-        # proc.kill()
+# proc.wait()
+# proc.kill()
